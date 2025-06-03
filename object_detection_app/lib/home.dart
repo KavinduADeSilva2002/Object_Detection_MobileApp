68791f6b92a6dc37a8d56838bb98d45140326116
Kavindu Anjana De Silva <kavinduanjana35@gmail.com>
2025-06-03 08:59:32 +0530

    update home.dart

--- object_detection_app/lib/home.dart
+++ object_detection_app/lib/home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:tflite/tflite.dart';
 import 'main.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({super.key});
 
   @override
   _HomeState createState() => _HomeState();
@@ -15,6 +16,8 @@ class _HomeState extends State<Home> {
   String result = '';
   CameraController? cameraController;
   CameraImage? imgCamera;
+
+  get response => null;
 
   initCamera() {
     cameraController = CameraController(cameras![0], ResolutionPreset.medium);
@@ -32,8 +35,82 @@ class _HomeState extends State<Home> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model.tflite",
+      labels: "assets/labelsFruits.txt",
+    );
   }
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadModel();
+  }
+
+  @override
+  Future<void> dispose() async {
+    super.dispose();
+
+    await Tflite.close();
+    cameraController!.dispose();
+  }
+
+  runModelOnStreamFrames() async {
+    var recognition = await Tflite.runModelOnFrame(
+      bytesList:
+          imgCamera!.planes.map((plane) {
+            return plane.bytes;
+          }).toList(),
+      imageHeight: imgCamera!.height,
+      imageWidth: imgCamera!.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      rotation: 90,
+      numResults: 2,
+      threshold: 0.1,
+      asynch: true,
+    );
+
+    result = '';
+    recognition!.forEach((element) {
+      result +=
+          response['label'] +
+          ' ' +
+          (response['confidence'] as double).toStringAsFixed(2) +
+          '\n\n';
+    });
+    setState(() {
+      result;
+    });
+
+    isWorking = false;
+  }
+@override
+Widget build(BuildContext context) {
+  return SafeArea(
+    child: Column(
+      children: [
+        AppBar(
+          title: Text('Fruits Detector App GetX'),
+          backgroundColor: Colors.redAccent,
+          centerTitle: true,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/back.jpg'),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
+
+  
+}
+
+
